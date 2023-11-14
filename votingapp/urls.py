c4294e2c4a7cbd195537c7d15d98f7df150a392e
l.ofili@rgu.ac.uk <l.ofili@rgu.ac.uk>
2023-11-14 08:53:50 +0000

    installed django bootstrap sidebar package for dashboard

--- votingapp/urls.py
+++ votingapp/urls.py
@@ -2,7 +2,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
 from . views import HomePage
+from django.views.generic import TemplateView
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
  path('', HomePage.as_view(), name="home"),
+ path('demo',TemplateView.as_view(template_name="bootstrap_base.html"),name='demo'),
+path('popovers',TemplateView.as_view(template_name="bootstrap_popovers.html"), name="popovers"),
+path('login',auth_views.LoginView.as_view(), name="login"),
 ]
